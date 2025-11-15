8a6f8d42810c48c0bff1da86aedb55a2596b3fc4
Verninii <lorenzzo.vernini@gmail.com>
2025-11-14 21:12:28 -0300

    atualizado smartwatch

---
+++ src/config/env.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+
+function required(name: string, value: string | undefined) {
+  if (!value) throw new Error(`Missing env var: ${name}`);
+  return value;
+}
+
+export const env = {
+  PORT: Number(process.env.PORT ?? 8080),
+  API_TOKEN: required("API_TOKEN", process.env.API_TOKEN),
+  MONGODB_URI: required("MONGODB_URI", process.env.MONGODB_URI),
+  CORS_ORIGIN: process.env.CORS_ORIGIN ?? "*",
+};
