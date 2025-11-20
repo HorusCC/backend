6567eec35b48963d4ee26ba921b4cf60820c0538
Verninii <lorenzzo.vernini@gmail.com>
2025-11-20 12:42:42 -0300

    Revert "voltando a funcionalidade"
    This reverts commit 986d7c40995897b2cb72289492bf2c1042890b75.

--- src/routes/routesAPI.ts
+++ src/routes/routesAPI.ts
@@ -409,12 +409,16 @@ export async function userRoutes(
           const token = params.get("token");
           const btn = document.getElementById("btn");
           const msg = document.getElementById("msg");
-          const appLink = document.getElementById("appLink");
+
+         if (!resp.ok) {
+  msg.textContent = data.message || "Erro ao redefinir senha.";
+  msg.style.color = "#F87171";
+  btn.disabled = false;
+  return;
+}
+
 msg.textContent = data.message || "Senha redefinida com sucesso!";
 msg.style.color = "#10B981";
-if (appLink) {
-  appLink.style.display = "block";
-}
 
           btn.addEventListener("click", async () => {
             const pass = (document.getElementById("password")).value;
@@ -457,7 +461,6 @@ if (appLink) {
 
               msg.textContent = data.message || "Senha redefinida com sucesso!";
               msg.style.color = "#10B981";
-              appLink.style.display = "block";
             } catch (e) {
               msg.textContent = "Erro de conexão. Tente novamente.";
               msg.style.color = "#F87171";
